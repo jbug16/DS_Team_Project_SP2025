1c7a63c787680a1a4f423ea77da6b48f0b369be9
Christian10951 <86986222+Christian10951@users.noreply.github.com>
2025-04-12 15:19:42 -0400

    Update graph.h
    - Changed private function name from Node to Airport
    - Added public functions for the graph class

--- DS_Team_Project/graph.h
+++ DS_Team_Project/graph.h
@@ -1,20 +1,30 @@
-//
-// Created by jenna on 4/12/2025.
-//
-
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <edge.h>
+#include "airport.h"
+#include "edge.h"
 #include <vector>
-using namespace std;
 
 class Graph {
-  private:
-    vector<Node> nodes;
-    vector<Edge> edges;
-  public:
+private:
+    struct Node {
+        Airport airport;
+        vector<Edge> edges;
+    };
 
+    vector<Node> adjacencyList;
+
+public:
+    Graph();
+
+    void addAirport(const Airport& ap);
+    void addFlight(string origin, const Edge& edge);
+
+    int findAirportIndex(string code) const;
+    bool airportExists(string code) const;
+
+    void printGraph() const;
+    
 };
 
-#endif //GRAPH_H
+#endif
